bbbb1f0cb3652b279ec892872630b8b1dcceccf0
visionbreaks <49587941+visionbreaks@users.noreply.github.com>
2019-04-16 17:30:44 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 
+var reasons = ['Chick flicks make me emotional',
+              'I love the Kardashians',
+              'Can we just lay around in sweats and watch \'the notebook\'?',
+              'I love crossfit and diet shakes',
+              'I love pinning cute clothes',
+              'Can we get BDubs?  It\'s our cheat day right?',
+              'I love binging Greys Anatomy in my PJ\'s',
+              'I\'ll agree with your parents']
+function new_reason(){
+var random_number = Math.floor(Math.random()*(reasons.length));
+}
+document.getElementByID('reason_display').innerHTML = reasons[random_number]
