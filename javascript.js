63e6998b2b9c83cfa2fbd2c7ec7c4aa8b8275cbd
visionbreaks <49587941+visionbreaks@users.noreply.github.com>
2019-04-16 17:43:34 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,5 +11,6 @@ function new_reason(){
 var random_number = Math.floor(Math.random()*(reasons.length));
 }
 
-var pics = [<img src = ''>]
+var pics = [<img src = 'https://user-images.githubusercontent.com/49587941/56246047-ec619d00-606e-11e9-9a58-8f52023cfb0e.jpg'>,
+           img src = '']
 document.getElementById('reason_display').innerHTML = reasons[random_number]
